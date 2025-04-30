9e951c3ad5335285118243ee4cce70ab5efaaa8e
Jose Diego Moreno <jdmorenopulido@gmail.com>
2025-04-30 10:32:36 +0200

    Finished notifications page

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -1,31 +1,36 @@
-import React from 'react'
-import CreatePost from '../components/create-post/create-post'
-import PostList from '../components/posts-list/posts-list'
+import React from "react";
+import CreatePost from "../components/create-post/create-post";
+import PostList from "../components/posts-list/posts-list";
 import { createClient } from "@/utils/supabase/server";
-import { redirect } from 'next/navigation';
-import style from './notifications.module.css'
+import { redirect } from "next/navigation";
+import style from "./notifications.module.css";
 async function NotificationsPage() {
-      const supabase = createClient();
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await (await supabase).auth.getUser();
+  
+  if (!user) {
+    redirect("/login");
+  }
+
+  const username = user.user_metadata.preferred_username;
+  
+  const { data: posts } = await (await supabase)
+    .from("posts")
+    .select("*, user:users(name, avatar_url, user_name)")
+    .textSearch("content", `@${username}`)
+    .order("created_at", { ascending: false });
+
     
-      const {
-        data: { user },
-      } = await (await supabase).auth.getUser();
-    
-      if (!user) {
-        redirect("/login");
-      }
-    
-      const { data: posts } = await (await supabase)
-        .from("posts")
-        .select("*, user:users(name, avatar_url, user_name)")
-        
-        .order("created_at", { ascending: false });
-    
+
   return (
     <section className={style.centralSection}>
-    <PostList posts={posts}/>
-  </section>
-  )
+      <div className="border-b border-white/20"><h1 className="text-2xl font-bold text-center py-4">Notifications</h1></div>
+      <PostList posts={posts} />
+    </section>
+  );
 }
 
-export default NotificationsPage
+export default NotificationsPage;
